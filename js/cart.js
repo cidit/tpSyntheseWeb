d1376903f86a863116f57ba7e7ea44271bb01a32
cid <ciditworld@gmail.com>
2019-05-16 17:17:18 -0400

    finished cart + corrected index

--- js/cart.js
+++ js/cart.js
@@ -1,28 +1,30 @@
 $(document).ready(function(){
 
-  var tableBody = $('main table tbody');
-  var template = $('#itemcard');
-  var t2 = $('tr', template);
-  console.log(t2.get(0));
+  var $tableBody = $('main table tbody');
+  var template = document.getElementById('itemcard');
+  var content = template.content.querySelector('tr');
+
 
   for (var id in cart) {
-    var item = $.extend({}, template);
-    $(item).attr('id', id);
-    $("td", template).eq(0).text(id);
-    $("img", template).attr('src', "../img/items/" + id + ".jpg");
-    $("td", template).eq(2).text(cart[id]['nm']);
-    $("td", template).eq(3).text(cart[id]['prc']);
-    $("td", template).eq(4).text(cart[id]['qntt']);
-    $("button", template).on('click', remove(id));
-    console.log(id);
-    $(tableBody).append($(template));
+    var clone = content.cloneNode(true);
+    var $item = $(clone);
+    $("td", $item).eq(0).find('span').text(id);
+    $("td", $item).eq(2).find('h5').text(cart[id]['nm']);
+    $("td", $item).eq(3).find('span').text(cart[id]['prc']);
+    $("td", $item).eq(4).text(cart[id]['qntt']);
+    $("img", $item).attr('src', "../img/items/" + id + ".jpg");
+
+    $tableBody.append($item);
+    console.log($item);
   }
 
-  function remove(id){
-    $('#' + id).remove();
+  $("td button").click(function(){
+    var $tr = $(this).parent().parent();
+    var id = $('td', $tr).eq(0).find('span').text();
+    console.log(id);
     delete cart[id];
-    console.log(id);
-  }
-
+    save();
+    $tr.remove();
+  });
 
 });
