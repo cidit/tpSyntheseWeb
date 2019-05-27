ae857cc06153b64a910ecacf6b2b54eb874d9470
cid <ciditworld@gmail.com>
2019-05-27 14:19:18 -0400

    stuff

--- js/contact.jsx
+++ js/contact.jsx
@@ -18,52 +18,118 @@ class Form extends React.Component {
   }
 
   handleEvent = (e) => {
-    const {name, value} = e.target;
+    const name = e.target.id;
+    const value = e.target.value;
     this.setState({[name]: value});
   }
 
   render() {
-    const {fn, ln, e, g, a, fi, m} = this.state;
-    const btn = {
-      submit: 'Soumettre',
-      reset: 'Réinitialiser',
-    };
-
     // attributes
     const attr = {
       fn: {
-
+        type: 'text',
+        id: 'firstname',
+        labelText: 'Prenom:',
+        placeholder: 'Felix',
+        required: 'required',
+        autofocus: 'autofocus',
       },
       ln: {
-
+        type: 'text',
+        id: 'lastname',
+        labelText: 'Nom:',
+        placeholder: 'St-Gelais',
+        required: 'required',
       },
       e: {
-
+        type: 'email',
+        id: 'email',
+        labelText: 'Adresse courriel:',
+        placeholder: 'a@b.com',
+        required: 'required',
       },
       g: {
-
+        r1: {
+          id: 'male',
+          labelText: ' Male',
+          value: 'M',
+          checked: 'checked',
+        },
+        r2: {
+          id: 'female',
+          labelText: ' Femelle',
+          value: 'F',
+        },
+        type: 'radio',
+        name: 'gender',
       },
       a: {
-
+        type: 'number',
+        id: 'age',
+        labelText: 'Age:',
+        placeholder: '17',
+        value: '17',
       },
       fi: {
-
+        id: 'famillialincome',
+        labelText: 'Revenu famillial:',
+        options: {
+          _1:{
+            value: '[1-25k[',
+            text: '1$ a 24 999$',
+          },
+          _2: {
+            value: '[25k-50k[',
+            text: '25 000$ a 49 999$',
+          },
+          _3: {
+            value: '[50k-70k[',
+            text: '50 000$ a 69 999$',
+            selected: 'selected',
+          },
+          _4: {
+            value: '[70k-100k[',
+            text: '70 000$ a 99 999$',
+          },
+          _5: {
+            value: '[100k-500k[',
+            text: '100 000$ a 499 999$',
+          },
+          _6: {
+            value: '500k+',
+            text: '500 000$ +',
+          },
+        },
       },
       m: {
-
+        id: 'message',
+        labelText: 'Message:',
+        placeholder: 'Votre message',
+        rows: '8',
+        required: 'required',
+      },
+      s: {
+        text: 'Soumettre',
+        id: 'submit',
+        type: 'submit',
+      },
+      r: {
+        text: 'Reinitialiser',
+        id: 'reset',
+        type: 'reset',
       },
     };
     return(
-      <form action="mailto:a@b.com" method="post">
-        <Input state={fn} type="text" meta={tmp} />
-        <Input state={ln} type="text" meta={tmp} />
-        <Input state={e} type="email" meta={tmp} />
-        <Input state={g} type="radio" meta={tmp} />
-        <Input state={a} type="number" meta={tmp} />
-        <Select state={fi} meta={tmp} />
-        <TextArea state={m} meta={tmp} />
-        <Button info={btn['submit']} />
-        <Button info={btn['reset']} />
+      <form method="post">
+        <Input meta={attr['fn']} />
+        <Input meta={attr['ln']} />
+        <Input meta={attr['e']} />
+        <Input meta={attr['g']} />
+        <Input meta={attr['a']} />
+        <Select meta={attr['fi']} />
+        <TextArea meta={attr['m']} />
+        <Button meta={attr['s']} />
+        <Button meta={attr['r']} />
       </form>
     )
   }
@@ -77,24 +143,26 @@ class Input extends React.Component {
   render() {
     const meta = this.props.meta;
     let myStatus;
-    if (meta.type = 'radio') {
+    if (meta.type == 'radio') {
+      const {r1, r2, ...misc} = meta;
       myStatus = (
         <div>
           <div>
-            <label></label>
-            <input type="" name="" value="" />
+            <input id={r1.id} value={r1.value} checked={r1.checked} {...misc} />
+            <label htmlFor={r1.id}>{r1.labelText}</label>
           </div>
           <div>
-            <label></label>
-            <input type="" name="" value="" />
+            <input id={r2.id} value={r2.value} {...misc} />
+            <label htmlFor={r2.id}>{r2.labelText}</label>
           </div>
         </div>
       )
     } else {
+      const {type, id, labelText, ...misc} = meta;
       myStatus = (
         <div>
-          <label></label>
-          <input type="" name="" value="" />
+          <label htmlFor={id} >{labelText}</label>
+          <input id={id} type={type} {...misc} />
         </div>
       )
     }
@@ -104,7 +172,6 @@ class Input extends React.Component {
 
 class Select extends React.Component {
   render() {
-    const info = this.props.info;
     const meta = this.props.meta;
     return(
       <div className="form-group">
@@ -116,14 +183,11 @@ class Select extends React.Component {
 
 class TextArea extends React.Component {
   render() {
-    const state = this.props.state;
-    const meta = this.props.meta;
-    var id, message;
-    state.map(([key, value]) => {id = key; message = value});
+    const {id, labelText, ...misc} = this.props.meta;
     return(
       <div className="form-group">
-        <label for={id}>Message</label>
-        <textarea class="form-control" id={id} value={message} {... meta}></textarea>
+        <label htmlFor={id}>{labelText}</label>
+        <textarea className="form-control" id={id} {...misc}></textarea>
       </div>
     )
   }
@@ -131,11 +195,9 @@ class TextArea extends React.Component {
 
 class Button extends React.Component {
   render() {
-    const info = this.props.info;
-    var t, v;
-    info.map(([key, value]) => {t = key; v = value});
+    const {text, ...misc} = this.props.meta;
     return(
-      <button className="btn btn-primary" type={t}>{v}</button>
+      <button className="btn btn-primary" {...misc}>{text}</button>
     )
   }
 }
